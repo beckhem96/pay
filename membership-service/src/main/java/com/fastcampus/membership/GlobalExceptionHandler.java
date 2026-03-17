3012125e4be20bead40189712620f161f73f5cc4
beckhem96 <beckhem96@naver.com>
2026-03-17 15:19:17 +0900

    k8s 설정 및 로그 설정

--- membership-service/src/main/java/com/fastcampus/membership/GlobalExceptionHandler.java
+++ membership-service/src/main/java/com/fastcampus/membership/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fastcampus.membership;
 
 import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
@@ -11,19 +12,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final Logger log = (Logger) LoggerFactory.getLogger(GlobalExceptionHandler.class);
+//    private static final Logger log = (Logger) LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(HttpServletRequest request, Exception e) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorLocation = "Unknown";
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        if (stackTrace != null && stackTrace.length > 0) {
+            StackTraceElement element = stackTrace[0];
+            // 예: "com.fc.membership.controller.MembershipController:42"
+            errorLocation = element.getClassName() + ":" + element.getLineNumber();
+        }
+
         boolean isDbError = e.getCause() instanceof SQLException; // DB 에러 판별 로직
 
         // MDC에 데이터를 넣으면 LogstashEncoder가 알아서 JSON 필드로 만들어줍니다!
         MDC.put("level", "ERROR");
         MDC.put("path", request.getRequestURI());
-        MDC.put("status", "500");
-        MDC.put("error_class", e.getClass().getSimpleName());
+        MDC.put("status", String.valueOf(httpStatus.value()));
+        MDC.put("error_location", errorLocation);
+        MDC.put("error_type", e.getClass().getSimpleName());
         MDC.put("is_db_error", String.valueOf(isDbError));
 
         log.error("서버 내부 에러 발생: {}", e.getMessage(), e);
--- membership-service/src/main/java/com/fastcampus/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/fastcampus/membership/adapter/in/web/RegisterMembershipController.java
@@ -4,6 +4,7 @@ import com.fastcampus.common.WebAdapter;
 import com.fastcampus.membership.application.port.in.RegisterMembershipCommand;
 import com.fastcampus.membership.application.port.in.RegisterMembershipUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisterMembershipController {
 
     private final RegisterMembershipUseCase registerMembershipUseCase;
+
+    @GetMapping("/test-error")
+    public String testError() {
+        throw new RuntimeException("K8s 에러 테스트!!!");
+    }
 
     @PostMapping("/membership/register")
     com.fastcampus.membership.domain.Membership register(@RequestBody RegisterMembershipRequest request) {
